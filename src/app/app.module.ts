52513ec5ef9ca02d91bc52ebe9dde4babfb19b3e
bhoopendra <bhoopendra.akgec@gmail.com>
2018-02-07 10:19:17 +0530

    Adding a project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import {HomeComponent} from './app.components.component.home';
 import {ProductList} from './app.component.productList';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, FormsModule],
   declarations: [ AppComponent,HomeComponent,ProductList],
   bootstrap:    [ AppComponent, HomeComponent,ProductList],
 })
